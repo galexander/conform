48436ee2a2ff4033300d3dc9403f55ed502660d9
Gavin Alexander <gavin.alexander@gmail.com>
2012-08-29 09:49:37 -0400

    update API

--- lib/Conform/Site/API.pm
+++ lib/Conform/Site/API.pm
@@ -7,12 +7,9 @@ requires 'uri';
 requires 'root';
 requires 'dir_list';
 requires 'file_open';
+requires 'file_read';
 requires 'file_close';
 requires 'nodes';
-requires 'classes';
-requires 'tasks';
-requires 'actions';
-requires 'vars';
 
 
 1;
