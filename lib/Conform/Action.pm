f7efcbbd97304e12c76845c536c91f3016affb3f
Gavin Alexander <gavin.alexander@gmail.com>
2013-02-04 23:36:49 -0500

    Support for dependencies

--- lib/Conform/Action.pm
+++ lib/Conform/Action.pm
@@ -1,5 +1,8 @@
 package Conform::Action;
-use Mouse::Role;
+use Mouse;
+use Conform::Logger qw($log);
+use Data::Dump qw(dump);
+use attributes;
 
 with 'Conform::Directive';
 
@@ -19,17 +22,55 @@ use Conform::Action;
 
 =head2   name
 
-L<Conform::Directive::name>
-
 =cut
 
 =head2   desc
 
-L<Conform::Directive::desc>
-
 =cut
 
-requires 'execute';
+has 'id' => (
+    is => 'rw',
+    isa => 'Str',
+);
+
+has 'complete' => (
+    is => 'rw',
+    isa => 'Bool',
+);
+
+has 'dependencies' => (
+    is => 'rw',
+    isa => 'ArrayRef',
+    default => sub { [] },
+);
+
+sub BUILD {
+    my $self = shift;
+    $self;
+}
+
+sub satisfies {
+    my $self = shift;
+    my $dependency = shift;
+    my $checked = 0;
+    if (exists $dependency->{name}) {
+        $checked++; 
+        if ($self->name ne $dependency->{name}) {
+            return 0;
+        }
+    }
+    if (exists $dependency->{id}) {
+        $checked++;
+        if (defined $self->id) {
+            if ($self->id ne $dependency->{id}) {
+                return 0;
+            }
+        }
+    }
+
+    return $checked;
+}
+
 
 =head1  SEE ALSO
 
