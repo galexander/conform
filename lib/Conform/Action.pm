0d489f7c3a718ae5087e7adcc47944e770f3bd15
Gavin Alexander <gavin.alexander@gmail.com>
2013-02-13 07:54:54 -0500

    Add some debugging and rework dependency satisfaction

--- lib/Conform/Action.pm
+++ lib/Conform/Action.pm
@@ -1,8 +1,8 @@
 package Conform::Action;
 use Mouse;
-use Conform::Logger qw($log);
 use Data::Dump qw(dump);
-use attributes;
+use Conform::Logger qw($log);
+use Conform::Debug qw(Trace Debug);
 
 =head1  NAME
 
@@ -119,14 +119,33 @@ has 'impl' => (
 
 =cut
 
-sub execute {
-    my $self = shift;
+sub execute { Trace;
+    my $self     = shift;
+
+    Debug "Executing Action (id=%s,name=%s,args=%s)",
+          $self->id,
+          $self->name,
+          dump($self->args);
+
+
     my $function = $self->impl;
     my @result   = $function->($self->args,
                                $self,
                                @_);
+
+    Debug "Action (id=%s,name=%s,args=%s) returned %s",
+          $self->id,
+          $self->name,
+          dump($self->args),
+          dump(\@result);
+
     $self->result(\@result);
+
     $self->complete(1);
+
+    return wantarray
+            ? @result
+            :\@result;
 }
 
 =item B<satisfies>
@@ -135,26 +154,99 @@ sub execute {
 
 =cut
 
-sub satisfies {
-    my $self = shift;
-    my $dependency = shift;
-    my $checked = 0;
-    if (exists $dependency->{name}) {
-        $checked++; 
-        if ($self->name ne $dependency->{name}) {
-            return 0;
+
+sub _check_args {
+    my ($dependency, $args) = @_;
+
+    return 0 unless defined $dependency and
+                    defined $args;
+
+    if (!ref $dependency) {
+        if (!ref $args) {
+            return 1 if $args eq $dependency;
         }
-    }
-    if (exists $dependency->{id}) {
-        $checked++;
-        if (defined $self->id) {
-            if ($self->id ne $dependency->{id}) {
-                return 0;
+
+        if (ref $args eq 'HASH') {
+            for my $value (grep !ref, values %$args) {
+                if ($value eq $dependency) {
+                    return 1;
+                }
+            }
+        }
+        if (ref $args eq 'ARRAY') {
+            if (grep /^\Q$dependency\E$/, @$args) {
+                return 1;
+            }
+        }
+
+    } else {
+
+        if (ref $dependency eq 'HASH') {
+
+            if (!ref $args) {
+                return 1 if grep /^\Q$dependency\E$/, values %$args;
+            }
+
+            if (ref $args eq 'HASH') {
+                for my $check (keys %$dependency) {
+                    return 1
+                        if (exists $args->{$check} &&
+                           ($args->{$check} eq $dependency->{$check}));
+                }
+            }
+
+            if (ref $args eq 'ARRAY') {
+                for my $check (values %$dependency) {
+                    return 1
+                        if grep /^\Q$check\E$/, values %$args;
+                }
             }
         }
     }
 
-    return $checked;
+    return 0;
+}
+
+sub satisfies { Trace;
+    my $self       = shift;
+    my $dependency = shift;
+
+    Debug "satisfies(%s)", dump($dependency);
+
+    if (ref $dependency eq 'HASH') {
+
+        for my $check (keys %$dependency) {
+
+            if ($check =~ /^action\.(\S+)/) {
+                my $param = $1;
+                if ($self->can($param) && 
+                   ($dependency->{$check} eq $self->$param())) {
+
+                    Debug "Action (id=%s,name=%s,args=%s) satisfies dependency %s",
+                          $self->id,
+                          $self->name,
+                          dump($self->args),
+                          dump($dependency);
+
+                    return 1;
+                }
+            }
+        }
+    }
+
+    if (_check_args $dependency, $self->args) {
+        Debug "Action (id=%s,name=%s,args=%s) satisfies dependency %s",
+              $self->id,
+              $self->name,
+              dump($self->args),
+              dump($dependency);
+    
+            return 1;
+    }
+
+
+    Debug "dependency (%s) - not satisfied";
+    return 0;
 }
 
 =back
